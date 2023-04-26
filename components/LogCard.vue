e6060b0686df3a20a0ffbda31aeba42605a675bc
Christine Nyries Yambao <yambao.cn@gmail.com>
2023-04-26 22:52:20 +0800

    fix: hydration mismatch error
    because I have some nested NuxtLinks

--- components/LogCard.vue
+++ components/LogCard.vue
@@ -26,23 +26,23 @@ defineProps({
 
 <template>
   <div class="border-2 rounded-lg overflow-hidden hover:bg-gray-50">
-    <NuxtLink :to="{ name: 'logs-slug', params: { slug } }">
-      <div class="p-6">
-        <div class="flex items-center flex-wrap gap-2">
-          <AppBadge v-for="(tag, index) in tags" :key="index" :name="tag" />
-        </div>
-
-        <h4 class="my-2 font-semibold text-2xl leading-tight">
-          {{ title }}
-        </h4>
-
-        <div class="text-gray-600 text-sm text-right">
-          <span>published <AppDate :utc="published" /></span>
-          <span v-if="lastEdited"
-            >, last edited <AppDate :utc="lastEdited"
-          /></span>
-        </div>
+    <div class="p-6">
+      <div class="flex items-center flex-wrap gap-2">
+        <AppBadge v-for="(tag, index) in tags" :key="index" :name="tag" />
       </div>
-    </NuxtLink>
+
+      <h4 class="my-2 font-semibold text-2xl leading-tight">
+        <NuxtLink :to="{ name: 'logs-slug', params: { slug } }">
+          {{ title }}
+        </NuxtLink>
+      </h4>
+
+      <div class="text-gray-600 text-sm text-right">
+        <span>published <AppDate :utc="published" /></span>
+        <span v-if="lastEdited"
+          >, last edited <AppDate :utc="lastEdited"
+        /></span>
+      </div>
+    </div>
   </div>
 </template>
--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -21,25 +21,25 @@ defineProps({
 
 <template>
   <div class="border-2 rounded-lg overflow-hidden hover:bg-gray-50 w-72">
-    <NuxtLink :to="{ name: 'projects-slug', params: { slug } }">
-      <div class="p-6">
-        <img
-          class="border w-full"
-          :src="image"
-          :alt="`${title} project's image`"
-        />
-        <h4 class="my-4 font-semibold text-2xl leading-tight">
+    <div class="p-6">
+      <img
+        class="border w-full"
+        :src="image"
+        :alt="`${title} project's image`"
+      />
+      <h4 class="my-4 font-semibold text-2xl leading-tight">
+        <NuxtLink :to="{ name: 'projects-slug', params: { slug } }">
           {{ title }}
-        </h4>
-        <div class="flex items-center flex-wrap gap-2">
-          <AppBadge
-            v-for="(tag, index) in tags"
-            :key="index"
-            :name="tag"
-            route-name="projects"
-          />
-        </div>
+        </NuxtLink>
+      </h4>
+      <div class="flex items-center flex-wrap gap-2">
+        <AppBadge
+          v-for="(tag, index) in tags"
+          :key="index"
+          :name="tag"
+          route-name="projects"
+        />
       </div>
-    </NuxtLink>
+    </div>
   </div>
 </template>

320d71df6686d7f9cf657b052e999baa56550190
Christine Nyries Yambao <yambao.cn@gmail.com>
2023-04-27 14:58:57 +0800

    fix: possibly logs and projects list not showing on first load in prod - try#2

--- pages/index.vue
+++ pages/index.vue
@@ -10,26 +10,9 @@ const route = useRoute();
 const env = useEnvVariables();
 const storyblokApi = useStoryblokApi();
 
+let totalPages = 0;
 const perPage = 10;
 const page = ref(Number(route.query.page) || 1);
-const tags = route.query.with_tag;
-const searchTerm = route.query.search_term;
-const isDev = route.query._storyblok || env.isDev;
-
-const stories = ref<ISbStoryData[]>([]);
-const { data } = await useAsyncData("logs", async () => {
-  return await storyblokApi.get("cdn/stories/", {
-    starts_with: "logs/",
-    page: page.value,
-    per_page: perPage,
-    ...(typeof tags === "string" && { with_tag: tags }),
-    ...(typeof searchTerm === "string" && { search_term: searchTerm }),
-    version: isDev ? "draft" : "published",
-  });
-});
-const totalPages = Math.ceil((data.value?.total || 0) / perPage);
-stories.value = data.value?.data.stories || [];
-
 const updatePage = () => {
   route.name &&
     router.push({
@@ -37,31 +20,50 @@ const updatePage = () => {
       query: { ...route.query, page: page.value },
     });
 };
+
+const tags = route.query.with_tag;
+const searchTerm = route.query.search_term;
+const isDev = route.query._storyblok || env.isDev;
+
+const stories = ref<ISbStoryData[]>([]);
+
+onMounted(async () => {
+  const { data } = await useAsyncData("logs", async () => {
+    return await storyblokApi.get("cdn/stories/", {
+      starts_with: "logs/",
+      page: page.value,
+      per_page: perPage,
+      ...(typeof tags === "string" && { with_tag: tags }),
+      ...(typeof searchTerm === "string" && { search_term: searchTerm }),
+      version: isDev ? "draft" : "published",
+    });
+  });
+  totalPages = Math.ceil((data.value?.total || 0) / perPage);
+  stories.value = data.value?.data.stories || [];
+});
 </script>
 
 <template>
-  <div>
-    <div v-show="stories.length > 0" class="flex flex-col gap-y-6">
-      <LogCard
-        v-for="story in stories"
-        :key="story.content._uid"
-        :slug="story.slug"
-        :published="story.first_published_at || story.created_at"
-        :last-edited="story.published_at || undefined"
-        :tags="story.tag_list"
-        :title="story.content.title"
-      />
-      <v-pagination
-        v-model="page"
-        :pages="totalPages"
-        :range-size="2"
-        active-color="#F9FAFB"
-        @update:model-value="updatePage"
-      />
-    </div>
-    <div v-show="stories.length === 0">
-      <p class="italic text-lg text-center">Will publish something soon.</p>
-    </div>
+  <div v-if="stories.length > 0" class="flex flex-col gap-y-6">
+    <LogCard
+      v-for="story in stories"
+      :key="story.content._uid"
+      :slug="story.slug"
+      :published="story.first_published_at || story.created_at"
+      :last-edited="story.published_at || undefined"
+      :tags="story.tag_list"
+      :title="story.content.title"
+    />
+    <v-pagination
+      v-model="page"
+      :pages="totalPages"
+      :range-size="2"
+      active-color="#F9FAFB"
+      @update:model-value="updatePage"
+    />
+  </div>
+  <div v-else>
+    <p class="italic text-lg text-center">Will publish something soon.</p>
   </div>
 </template>
 
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -10,36 +10,34 @@ const tags = route.query.with_tag;
 const searchTerm = route.query.search_term;
 
 const stories = ref<ISbStoryData[]>([]);
-const { data } = await useAsyncData(
-  "projects",
-  async () =>
-    await api.get("cdn/stories/", {
-      starts_with: "projects/",
-      ...(typeof tags === "string" && { with_tag: tags }),
-      ...(typeof searchTerm === "string" && { search_term: searchTerm }),
-      version: isDev ? "draft" : "published",
-    })
-);
-stories.value = data.value?.data.stories || [];
+
+onMounted(async () => {
+  const { data } = await useAsyncData(
+    "projects",
+    async () =>
+      await api.get("cdn/stories/", {
+        starts_with: "projects/",
+        ...(typeof tags === "string" && { with_tag: tags }),
+        ...(typeof searchTerm === "string" && { search_term: searchTerm }),
+        version: isDev ? "draft" : "published",
+      })
+  );
+  stories.value = data.value?.data.stories || [];
+});
 </script>
 
 <template>
-  <div>
-    <div
-      v-show="stories.length > 0"
-      class="flex gap-6 flex-wrap justify-center"
-    >
-      <ProjectCard
-        v-for="story in stories"
-        :key="story.content._uid"
-        :slug="story.slug"
-        :tags="story.tag_list"
-        :title="story.content.title"
-        :image="story.content.image.filename"
-      />
-    </div>
-    <div v-show="stories.length === 0">
-      <p class="italic text-lg text-center">Will build something soon.</p>
-    </div>
+  <div v-if="stories.length > 0" class="flex gap-6 flex-wrap justify-center">
+    <ProjectCard
+      v-for="story in stories"
+      :key="story.content._uid"
+      :slug="story.slug"
+      :tags="story.tag_list"
+      :title="story.content.title"
+      :image="story.content.image.filename"
+    />
+  </div>
+  <div v-else>
+    <p class="italic text-lg text-center">Will build something soon.</p>
   </div>
 </template>

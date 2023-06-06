e2c44e120389e421c7f373b53112cd2ddfba504b
Christine Nyries Yambao <yambao.cn@gmail.com>
2023-06-07 04:38:42 +0800

    feat: pagination on logs page
    fixes #1

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,12 @@ const route = useRoute();
 const storyblokApi = useStoryblokApi();
 const isDraft = route.query._storyblok || useRuntimeConfig().isDev;
 
-const perPage = 10;
+const perPage = 3;
 const page = ref(Number(route.query.page) || 1);
 
 const getFilters = (query) =>
   Object.keys(query).reduce((_filters, key) => {
-    if (["with_tag", "search_term"].includes(key)) {
+    if (["with_tag", "search_term", "page"].includes(key)) {
       _filters[key] = query[key];
     }
     return _filters;
@@ -19,30 +19,50 @@ const { data, refresh } = await useAsyncData(route.fullPath, () => {
   return storyblokApi.get("cdn/stories/", {
     is_startpage: 0,
     starts_with: "logs/",
-    page: page.value,
     per_page: perPage,
     ...filters,
     version: isDraft ? "draft" : "published",
   });
 });
 const logs = computed(() => data.value?.data.stories);
-
+const total = computed(() => data.value?.total);
+const pages = computed(() => Math.ceil(total.value / perPage));
 watch(
   () => route.query,
   () => refresh()
 );
+watch(page, (newValue) => navigateTo(`?page=${newValue}`));
 </script>
 
 <template>
-  <div v-if="logs" class="flex flex-col gap-8">
-    <LogCard
-      v-for="log in logs"
-      :key="log.id"
-      :log="log.content"
-      :slug="log.slug"
-      :published="log.first_published_at || log.created_at"
-      :last-edited="log.published_at"
-      :tags="log.tag_list"
-    />
+  <div>
+    <div v-if="logs" class="flex flex-col gap-8">
+      <LogCard
+        v-for="log in logs"
+        :key="log.id"
+        :log="log.content"
+        :slug="log.slug"
+        :published="log.first_published_at || log.created_at"
+        :last-edited="log.published_at"
+        :tags="log.tag_list"
+      />
+    </div>
+    <div v-if="pages > 1" class="mt-10 flex justify-center gap-4">
+      <button
+        class="w-[100px] rounded border py-2 shadow enabled:hover:bg-gray-50 disabled:bg-slate-300"
+        :class="{}"
+        :disabled="page > 1"
+        @click="page -= 1"
+      >
+        ⬅️ Prev
+      </button>
+      <button
+        class="w-[100px] rounded border py-2 shadow enabled:hover:bg-gray-50 disabled:bg-slate-300"
+        :disabled="page < pages"
+        @click="page += 1"
+      >
+        Next ➡️
+      </button>
+    </div>
   </div>
 </template>

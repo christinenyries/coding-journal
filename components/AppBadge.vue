6c0a11d303b7583853060a9747b9f94d3db0472d
Christine Nyries Yambao <yambao.cn@gmail.com>
2023-06-16 13:14:27 +0800

    fix: badges in logs/<slug> not working

--- components/AppBadge.vue
+++ components/AppBadge.vue
@@ -1,15 +1,19 @@
 <script setup>
-defineProps({
+const props = defineProps({
   name: {
     type: String,
     required: true,
   },
+  for: {
+    type: String,
+    default: "",
+  },
 });
-const { path } = useRoute();
+const path = computed(() => `${props.for}/?with_tag=${props.name}`);
 </script>
 
 <template>
-  <NuxtLink :to="`${/\/\w+/.exec(path) || ''}/?with_tag=${name}`">
+  <NuxtLink :to="path">
     <span
       class="inline-flex items-center rounded-full bg-gray-200 px-2 py-1 text-sm font-semibold text-gray-600 hover:bg-gray-300"
     >
